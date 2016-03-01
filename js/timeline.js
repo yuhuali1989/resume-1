c06d64ab9640077d418484e78ed5666e2d3eedfe
yuhuali <yuhuali@smartdevices.com.cn>
2016-03-01 17:18:55 +0800

    last version

--- js/timeline.js
+++ js/timeline.js
@@ -1,19 +1,19 @@
 function change1() {
 				document.getElementById("timeline-circid1").className = "timeline-circles timeline-circle1change1";
 				document.getElementById("textcircid1").className = "timeline-textformat textincircle1and1";
-				document.getElementById("textcircid1").innerHTML = "SOOCHOW UNIVERSITY";
+				document.getElementById("textcircid1").innerHTML = "HADOOP";
 				document.getElementById("timeline-circid2").className = "timeline-circles timeline-circle2change1";
 				document.getElementById("textcircid2").className = "timeline-textformat textincircle2and1";
-				document.getElementById("textcircid2").innerHTML = "JavaScript";
+				document.getElementById("textcircid2").innerHTML = "STORM";
 				document.getElementById("timeline-circid3").className = "timeline-circles timeline-circle3change1";
 				document.getElementById("textcircid3").className = "timeline-textformat textincircle3and1";
-				document.getElementById("textcircid3").innerHTML = "HTML";
+				document.getElementById("textcircid3").innerHTML = "SPARK";
 				document.getElementById("timeline-circid4").className = "timeline-circles timeline-circle4change1";
 				document.getElementById("textcircid4").className = "timeline-textformat textincircle4and1";
-				document.getElementById("textcircid4").innerHTML = "C";
+				document.getElementById("textcircid4").innerHTML = "SHELL";
 				document.getElementById("timeline-circid5").className = "timeline-circles timeline-circle5change1";
 				document.getElementById("textcircid5").className = "timeline-textformat textincircle5and1";
-				document.getElementById("textcircid5").innerHTML = "CSS";
+				document.getElementById("textcircid5").innerHTML = "SPRING BOOT";
 				document.getElementById("timeline-circid6").className = "timeline-circles timeline-circle6change1";
 				document.getElementById("textcircid6").className = "timeline-textformat textincircle6and1";
 				document.getElementById("textcircid6").innerHTML = "";
@@ -21,39 +21,39 @@ function change1() {
 			function change2() {
 				document.getElementById("timeline-circid1").className = "timeline-circles timeline-circle1change2";
 				document.getElementById("textcircid1").className = "timeline-textformat textincircle1and2";
-				document.getElementById("textcircid1").innerHTML = "Java";
+				document.getElementById("textcircid1").innerHTML = "MAPREDUCE";
 				document.getElementById("timeline-circid2").className = "timeline-circles timeline-circle2change2";
 				document.getElementById("textcircid2").className = "timeline-textformat textincircle2and2";
-				document.getElementById("textcircid2").innerHTML = "C++";
+				document.getElementById("textcircid2").innerHTML = "HIVE DUF/DUAF";
 				document.getElementById("timeline-circid3").className = "timeline-circles timeline-circle3change2";
 				document.getElementById("textcircid3").className = "timeline-textformat textincircle3and2";
-				document.getElementById("textcircid3").innerHTML = "ASP.NET(C#)";
+				document.getElementById("textcircid3").innerHTML = "SHELL";
 				document.getElementById("timeline-circid4").className = "timeline-circles timeline-circle4change2";
 				document.getElementById("textcircid4").className = "timeline-textformat textincircle4and2";
-				document.getElementById("textcircid4").innerHTML = "SQL";
+				document.getElementById("textcircid4").innerHTML = "PYTHON";
 				document.getElementById("timeline-circid5").className = "timeline-circles timeline-circle5change2";
 				document.getElementById("textcircid5").className = "timeline-textformat textincircle5and2";
-				document.getElementById("textcircid5").innerHTML = "Data Structure";
+				document.getElementById("textcircid5").innerHTML = "SQL";
 				document.getElementById("timeline-circid6").className = "timeline-circles timeline-circle6change2";
 				document.getElementById("textcircid6").className = "timeline-textformat textincircle6and2";
-				document.getElementById("textcircid6").innerHTML = "jQuery";
+				document.getElementById("textcircid6").innerHTML = "VBA";
 			}
 			function change3() {
 				document.getElementById("timeline-circid1").className = "timeline-circles timeline-circle1change3";
 				document.getElementById("textcircid1").className = "timeline-textformat textincircle1and3";
-				document.getElementById("textcircid1").innerHTML = "Struts2";
+				document.getElementById("textcircid1").innerHTML = "VBA";
 				document.getElementById("timeline-circid2").className = "timeline-circles timeline-circle2change3";
 				document.getElementById("textcircid2").className = "timeline-textformat textincircle2and3";
-				document.getElementById("textcircid2").innerHTML = "HTML5";
+				document.getElementById("textcircid2").innerHTML = "SPRING MVC";
 				document.getElementById("timeline-circid3").className = "timeline-circles timeline-circle3change3";
 				document.getElementById("textcircid3").className = "timeline-textformat textincircle3and3";
-				document.getElementById("textcircid3").innerHTML = "jQuery UI";
+				document.getElementById("textcircid3").innerHTML = "EXCEL";
 				document.getElementById("timeline-circid4").className = "timeline-circles timeline-circle4change3";
 				document.getElementById("textcircid4").className = "timeline-textformat textincircle4and3";
-				document.getElementById("textcircid4").innerHTML = "CSS3";
+				document.getElementById("textcircid4").innerHTML = "CSS";
 				document.getElementById("timeline-circid5").className = "timeline-circles timeline-circle5change3";
 				document.getElementById("textcircid5").className = "timeline-textformat textincircle5and3";
-				document.getElementById("textcircid5").innerHTML = "Android";
+				document.getElementById("textcircid5").innerHTML = "JS";
 				document.getElementById("timeline-circid6").className = "timeline-circles timeline-circle6change3";
 				document.getElementById("textcircid6").className = "timeline-textformat textincircle6and3";
 				document.getElementById("textcircid6").innerHTML = "";
@@ -61,10 +61,10 @@ function change1() {
 			function change4() {
 				document.getElementById("timeline-circid1").className = "timeline-circles timeline-circle1change4";
 				document.getElementById("textcircid1").className = "timeline-textformat textincircle1and4";
-				document.getElementById("textcircid1").innerHTML = "Jqgrid";
+				document.getElementById("textcircid1").innerHTML = "EXCEL";
 				document.getElementById("timeline-circid2").className = "timeline-circles timeline-circle2change4";
 				document.getElementById("textcircid2").className = "timeline-textformat textincircle2and4";
-				document.getElementById("textcircid2").innerHTML = "Jquery EasyUI";
+				document.getElementById("textcircid2").innerHTML = "VBA";
 				document.getElementById("timeline-circid3").className = "timeline-circles timeline-circle3change4";
 				document.getElementById("textcircid3").className = "timeline-textformat textincircle3and4";
 				document.getElementById("textcircid3").innerHTML = "Spring MVC";
@@ -73,10 +73,10 @@ function change1() {
 				document.getElementById("textcircid4").innerHTML = "Ajax";
 				document.getElementById("timeline-circid5").className = "timeline-circles timeline-circle5change4";
 				document.getElementById("textcircid5").className = "timeline-textformat textincircle5and4";
-				document.getElementById("textcircid5").innerHTML = "Operating System";
+				document.getElementById("textcircid5").innerHTML = "CSS";
 				document.getElementById("timeline-circid6").className = "timeline-circles timeline-circle6change4";
 				document.getElementById("textcircid6").className = "timeline-textformat textincircle6and4";
-				document.getElementById("textcircid6").innerHTML = "ERP";
+				document.getElementById("textcircid6").innerHTML = "JS";
 			}
 
 window.onload = change1;
